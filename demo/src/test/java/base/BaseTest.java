d51792b3b7409daac009e3d9aa8c8a2f5a32e481
saurav <saurav.rimal@gmail.com>
2022-09-28 08:23:08 -0600

    changed in chromeoptions

--- demo/src/test/java/base/BaseTest.java
+++ demo/src/test/java/base/BaseTest.java
@@ -43,7 +43,7 @@ public class BaseTest {
         System.out.println("inside base test driverSetup");
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--no-sandbox");
+        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
 //        options.addArguments("--remote-debugging-port=9444");
 //        options.addArguments("--window-size=1920,1080");
